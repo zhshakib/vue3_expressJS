31bb7ba75ad20aa6764c7fbd580dd995b1d6f0bf
zhshakib <zhshakib@gmail.com>
2024-10-08 13:34:38 +0600

    chore: rearrange the files

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,13 +1,3 @@
-<template>
-  <div>
-    <h2>Home!</h2>
-    <p>Calling Backend! </p>
-  </div>
-  <h1>Message: {{ message.message }}</h1>
-  <h2>Name: {{ message.name }}</h2>
-  <h2>Age: {{ message.age }}</h2>
-</template>
-
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
@@ -21,3 +11,9 @@ axios.get('/').then((response) => {
 })
 
 </script>
+
+<template>
+  <h1>Message: {{ message.message }}</h1>
+  <h2>Name: {{ message.name }}</h2>
+  <h2>Age: {{ message.age }}</h2>
+</template>
